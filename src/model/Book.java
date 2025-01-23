67af7e0179476b0117b234a88815285f82feeae9
ArturM <arturmoskalenko74@gmail.com>
2025-01-23 20:44:25 +0100

    Start Project

---
+++ src/model/Book.java
@@ -0,0 +1,81 @@
+package model;
+
+import java.util.Date;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private Date releaseDate;
+    private Date returnDate;
+    private String nameOfCustomer;
+    // private Customer bookCustomer;(Возможно мы будем использовать сущность связь для сильной связи с книгой )
+
+
+    public Book(int id, String title, String author, Date releaseDate) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.releaseDate = releaseDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public String getNameOfCustomer() {
+        return nameOfCustomer;
+    }
+
+    public void setNameOfCustomer(String nameOfCustomer) {
+        this.nameOfCustomer = nameOfCustomer;
+    }
+
+    @Override
+    public String toString() {
+        return "Book:{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", releaseDate=" + releaseDate +
+                ", returnDate=" + returnDate +
+                ", nameOfCustomer='" + nameOfCustomer + '\'' +
+                '}';
+    }
+}
---
+++ src/model/Library.java
@@ -0,0 +1,7 @@
+package model;
+
+import utils.MyArrayList;
+
+public class Library {
+    private MyArrayList<Book> books = new MyArrayList<>();
+}
